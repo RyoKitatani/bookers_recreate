04b101bf17d877b77d8175d682edf2a199e1480e
RyoKitatani <ryo0929m@gmail.com>
2021-03-07 03:24:31 +0000

    投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book = Book.new
     @book = Book.find(params[:id])
+  end
+
+  def create
+   @book = Book.new(book_params) 
+   @book.save
+   redirect_to book_path(@book)
   end
 
   def edit
@@ -14,6 +22,9 @@ class BooksController < ApplicationController
   def update
   end
   
-  p
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "books#index"
-  resources :books, only:[:show, :edit, :update, :destory]
+  resources :books, only:[:show, :edit, :create, :update, :destory]
 end
